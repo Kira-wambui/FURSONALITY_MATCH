9aea2759c26a398a7795fed22cce69ad29a55eda
Kira-wambui <nancykira21@gmail.com>
2023-09-07 12:35:23 +0300

    furry

--- FURSONALITY_MATCH/FURSONALITY_MATCH/README.md
+++
@@ -1,3 +0,0 @@
-# FURSONALITY_MATCH
-This is an MVP of what the website would look like
-Its objective is to pair up humans to furry pets depending onntheir pesonality 
---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template  # Import render_template
+
+app = Flask(__name__)
+
+@app.route('/questionnaire')
+def questionnaire():
+    return render_template('questionnaire.html')  # Make sure to render the HTML templatecd
